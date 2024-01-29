2ff4a0fb56dbea817316bdb6bd76dd19b2801002
Hugo Messias <hugomessias9@gmail.com>
2024-01-29 00:00:02 -0300

    searchLines() now checks for red pixels on the image instead of previous approach
    getOcarinaState() now also checks for textbox when deciding if 'ocarina' should be False
    
    changed check for size of black bars: pushed to lower portion of the screen and reversed it's iteration, it now checks the black bar below the game screen instead of above, allowing one single screenshot to be used on both the check for size of black bar and the check for the textbox

--- mm.py
+++ mm.py
@@ -42,91 +42,63 @@ def searchBStop(haystack_img, needle_img):
     return False
 
 
-def searchLines(gray_img, thresh_val: float):
+def searchLines(image):
+    bin_img = np.array(image)
+    for i, line in enumerate(bin_img):
+        for j, pixel in enumerate(line):
+            # print(pixel)
+            if pixel[0] > 100 and pixel[1] < 50 and pixel[2] < 50:
+                bin_img[i][j] = [255, 255, 255]
+            else:
+                bin_img[i][j] = [0, 0, 0]
+    # cv.imwrite("other/__binary.png", bin_img)
+
     ofst = 0
-    kernel = cv.getStructuringElement(cv.MORPH_RECT, (40,5))
-    thresh = cv.threshold(gray_img, thresh_val, 255, cv.THRESH_BINARY)[1]
-    morph = cv.morphologyEx(thresh, cv.MORPH_RECT, kernel)
-    # cv.imwrite("other/__thresh.png", thresh)
-    # cv.imwrite("other/__morph.png", morph)
-    while ofst <= 280:
-        height = morph[0:, ofst:ofst+1]
-        # cv2.imwrite("other/__height.png", height)
+    while ofst <= 200:
+        height = bin_img[0:, ofst:ofst+1]
         lines = []
         line = [0, 0]
         get_line = 0
 
         # get occurences of possible lines on the image
+        if np.count_nonzero(height) < 40:
+            ofst += 10
+            continue
         for i, x in enumerate(height):
             # print(i, x)
             # input()
-            if not get_line and x[0]:
+            if not get_line and x[0][0]:
                 line[0] = i
                 line[1] = i
                 # print(line)
                 get_line = 1
-            elif get_line and not x[0]:
+            elif get_line and not x[0][0]:
                 line[1] = i-1
                 # print(line)
                 lines.append(line.copy())
                 get_line = 0
         # print(lines)
         if len(lines) < 4:
-            ofst += 20
+            ofst += 10
             continue
-        # limit line height to avoid catching huge white stains
-        remove = []
-        for i, line in enumerate(lines):
-            line_height = line[1]-line[0]
-            # print(line, line_height)
-            if line_height < 2 or line_height > 12:
-                remove.append(i)
-        for i, n in enumerate(remove):
-            lines.pop(n-i)
-        # print(ofst, "height lim: ", lines, len(lines))
 
-        # keep only lines within a limit of spacing between them
-        remove = []
-        for i, _ in enumerate(lines):
-            # print(lines[i])
-            if not i:
-                if len(lines) < 2:
-                    break
-                prv = lines[i][0] - 0
-                nxt = lines[i+1][0] - lines[i][1]
-            elif i == len(lines)-1:
-                prv = lines[i][0] - lines[i-1][1]
-                nxt = 280 - lines[i][1]
-            else:
-                prv = lines[i][0] - lines[i-1][1]
-                nxt = lines[i+1][0] - lines[i][1]
-            # print(prv, nxt, (prv < 18 or prv > 38) and (nxt < 18 or nxt > 38))
-            if (prv < 18 or prv > 38) and (nxt < 18 or nxt > 38):
-                remove.append(i)
-        for i, n in enumerate(remove):
-            lines.pop(n-i)
-        # print(ofst, "spacing lim: ", lines, len(lines))
-
-        # get density of white pixels do define if it's a vaid line
         density = 0
         while len(lines) == 4:
             rtrn = True
             for line in lines:
-                width = morph[line[0]:line[1], ofst:]
+                width = bin_img[line[0]:line[1], ofst:ofst+60]
                 # cv.imwrite("other/__width.png", width)
                 line_height = line[1]-line[0]
-                density = (np.count_nonzero(width))/(line_height*(400-ofst))
+                density = (np.count_nonzero(width)//3)/(line_height*60)
                 # print(ofst, line, density)
                 # input()
                 if density < 0.4:
                     rtrn = False
                     break
             if rtrn:
-                cv.imwrite("other/__morph.png", morph)
-                cv.imwrite("other/__gray.png", gray_img)
                 return True
             break
-        ofst += 20
+        ofst += 10
     return False
 
 
@@ -145,30 +117,36 @@ def getOcarinaState():
             if ocarina:
                 continue
 
-            screen = pyautogui.screenshot(region=(780, 750, 400, 280))
-            gray = cv.cvtColor(np.array(screen), cv.COLOR_RGB2GRAY)
-            for num in [20, 30, 40]:
-                ocarina = searchLines(gray, num)
+            screen = pyautogui.screenshot(region=(1000, 720, 180, 260))
+            searchLines(screen)
             time.sleep(wait)
         else:
-            screen = pyautogui.screenshot(region=(750, 0, 1, 240))
+            screen = pyautogui.screenshot(region=(785, 840, 1, 240))
             gray = cv.cvtColor(np.array(screen), cv.COLOR_RGB2GRAY)
             # cv.imwrite("__gray.png", gray)
-            for i, x in enumerate(gray):
+            textbox_check = [gray[0][0], None]
+            for i, x in enumerate(reversed(gray)):
                 if x[0]:
                     size = i
                     break
             while True: 
                 print(ocarina)   
-                screen = pyautogui.screenshot(region=(750, 0, 1, 240))
+                screen = pyautogui.screenshot(region=(785, 840, 1, 240))
                 gray = cv.cvtColor(np.array(screen), cv.COLOR_RGB2GRAY)
-                # cv.imwrite("__gray.png", gray)
-                for i, x in enumerate(gray):
+                # cv.imwrite("other/__gray.png", gray)
+                textbox_check[1] = gray[0][0]
+                if textbox_check[1] < textbox_check[0]//2:
+                    print("textbox")
+                    ocarina = False
+                    break
+                for i, x in enumerate(reversed(gray)):
+                    # print(i, x[0])
                     if x[0]:
                         new_size = i
                         break
-                # print(size, new_size)
+                print(size, new_size)
                 if size != new_size:
+                    print("size")
                     ocarina = False
                     break
                 time.sleep(wait)
